ae40ac4cd906639361a9c9a473b3105fbfb98b84
Marvin Filus <marvinfi@Marvins-MacBook-Air.local>
2017-04-16 14:01:54 -0400

    Hw

--- t-shirt.js
+++ t-shirt.js
@@ -1,11 +1,11 @@
-let buttons = document.querySelectorAll('button')
-	for (let i = 0; i < buttons.length; i++) {
-		let buttton = buttons[i];
-		button.addEventListener('click' , function(){
-			let whereTheNumberGoes = document.querySelector('.one')
-			whereTheNumberGoes.textContent = i + 1;
-		});
-	}
+// let buttons = document.querySelectorAll('button')
+// 	for (let i = 0; i < buttons.length; i++) {
+// 		let buttton = buttons[i];
+// 		button.addEventListener('click' , function(){
+// 			let whereTheNumberGoes = document.querySelector('.one')
+// 			whereTheNumberGoes.textContent = i + 1;
+// 		});
+// 	}
 <!-- front shirt blue -->
 function rotating(){
 	let element = document.querySelector('.frontblue');
@@ -59,16 +59,12 @@ function backrotation3(){
 
 buttontrio.addEventListener('click', backrotation3);
 
-function itemInShoppingCart(event){
-	 cartPicture = event.target.parentElement.parentElement;
-	 console.log('I know what I am doing');
-};
 // Changing the Shirt Sz Box =================================================
 
 function mainXl(event){
-	smallXl = event.target.parentElement.parentElement
+	smallXl = event.target.parentElement.parentElement;
 	bigXl = smallXl.closest('.shirt').querySelector('.sz');
-	console.log (bigXl , 'it worked !');
+	// console.log (bigXl , 'it worked !');
  let size = event.target.innerText;
 	bigXl.innerText = size
 }
@@ -76,4 +72,49 @@ let lis = document.querySelectorAll('li')
 for ( i = 0; i < lis.length; i++) {
 	let li = lis[i];
 li.addEventListener('click', mainXl);
+}
+
+// display shopping cart prices
+var showType = document.querySelector('.one');
+var showPrice = document.querySelector('.two');
+var cartIcon = document.querySelector('.theCart').parentElement.nextElementSibling;
+var cartType = cartIcon.querySelector('.type').innerHTML;
+var cartPrice = cartIcon.querySelector('.prices').innerHTML;
+var prices = []
+
+function moveToCart1 (){
+	let node = document.createElement("li");
+	let word = document.createTextNode('Save the Trees $29')
+	node.appendChild(word);
+	document.getElementById('list').appendChild(node);
+	prices.push(19);
+	console.log(prices);
+}
+
+function moveToCart2 (){
+	let node = document.createElement("li");
+	let word = document.createTextNode('Nature Lover $19')
+	node.appendChild(word);
+	document.getElementById('list').appendChild(node);
+	prices.push(19);
+	console.log(prices)
+}
+
+function moveToCart3 (){
+	let node = document.createElement("li");
+	let word = document.createTextNode('Forest Walk $39')
+	node.appendChild(word);
+	document.getElementById('list').appendChild(node);
+	prices.push(39);
+	console.log(prices);
+}
+
+function add(a, b) {
+    return a + b;
+}
+function theTotal (){
+	let totals = prices.reduce(add, 0);
+	console.log(prices.reduce(add, 0));
+	let placeTotal = document.getElementById("totalAmount")
+	placeTotal.innerHTML = totals;
 }
